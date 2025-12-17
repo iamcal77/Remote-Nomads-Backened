a6341704a37c751885fd20757a5852893c02e064
iamcal77 <calvinkipkirui99@gmail.com>
2025-12-17 13:51:22 +0300

    added careers site

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import auth, users, jobs, applications, candidates
 
 app = FastAPI(
@@ -6,6 +7,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://206.189.26.129:8080"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ✅ ROUTERS (leave as-is)
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["Users"])
 app.include_router(jobs.router, prefix="/api/v1/jobs", tags=["Jobs"])
