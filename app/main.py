28ce650f4173fc11c7dea0b71a1416476cacfcb8
iamcal77 <calvinkipkirui99@gmail.com>
2026-01-23 09:53:18 +0300

    added new endpoints

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "http://remote-nomads.206.189.26.129.nip.io"
+    "https://remote-nomads.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
