c3c358e84f7b20d3d9a9121170ed8e73fd27363d
iamcal77 <calvinkipkirui99@gmail.com>
2026-02-17 08:30:13 +0300

    added download cv

--- app/utils/file_upload.py
+++ app/utils/file_upload.py
@@ -1,16 +1,20 @@
-# app/utils/file_upload.py
-import os
-from fastapi import UploadFile
+from pathlib import Path
 from uuid import uuid4
 
+from fastapi import UploadFile
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 def save_file(file: UploadFile, folder: str = "uploads") -> str:
-    ext = os.path.splitext(file.filename)[1]
+    ext = Path(file.filename).suffix
     filename = f"{uuid4().hex}{ext}"
-    folder_path = os.path.join("static", folder)
-    os.makedirs(folder_path, exist_ok=True)
-    file_path = os.path.join(folder_path, filename)
+
+    folder_path = BASE_DIR / "static" / folder
+    folder_path.mkdir(parents=True, exist_ok=True)
+
+    file_path = folder_path / filename
 
     with open(file_path, "wb") as f:
         f.write(file.file.read())
 
-    return file_path
+    return f"static/{folder}/{filename}"
